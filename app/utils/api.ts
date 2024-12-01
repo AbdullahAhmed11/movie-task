8e466a19db9e2815a2e158f7ec9a843039843bf7
AbdullahAhmed11 <abdullah77zzx@gmail.com>
2024-12-01 04:39:09 +0200

    fix bug

--- app/utils/api.ts
+++ app/utils/api.ts
@@ -9,6 +9,7 @@ export const fetchMovies = async (query: string = '') => {
     const response = await axios.get(`https://cors-anywhere.herokuapp.com/https://freetestapi.com/api/v1/movies${query}`);;
     return response.data;
   } catch (error) {
+    console.log(error)
     throw new Error('Failed to fetch movies.');
   }
 };
